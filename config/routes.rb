39cd3a830faca3303ecad38998b1307aa3f37512
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-02 17:05:34 +0300

    Feat: added login & logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   # sign up
   post "/signup", to: "users#create"
   # auto-login
   get "/me", to: "users#show"
+
+  # Log in
+  post "/login", to: "sesions#create"
+  # Log out
+  delete "/logout", to: "sesions#destroy"
 end
